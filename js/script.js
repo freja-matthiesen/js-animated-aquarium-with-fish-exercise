bcb4eab41a29a538b4ddb913eeea83b373d97ca3
freja-matthiesen <frejamatthiesen@icloud.com>
2025-04-23 14:29:38 +0200

    3.3

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
 "use strict";
+document.addEventListener("DOMContentLoaded", function () {
+    // Vælg DOM-elementer
+    const anchor = document.querySelector(".anchor");
+    const anchorTooltip = document.getElementById("anchor-tooltip");
+    const audio = document.querySelector("audio");
+  });
