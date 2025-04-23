8e4cb286ba09b41f3ab52846b30fd21279367977
freja-matthiesen <frejamatthiesen@icloud.com>
2025-04-23 14:38:08 +0200

    4.4

--- js/script.js
+++ js/script.js
@@ -30,3 +30,21 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
   const tooltip = document.getElementById("tooltip");
+  fishInfo.forEach((fish) => {
+    const fishElem = document.querySelectorAll("." + fish.className);
+  
+    fishElem.forEach((el) => {
+      el.addEventListener("click", (e) => {
+        tooltip.innerText = `${fish.art} — Alder: ${fish.alder}`;
+        tooltip.style.top = `${e.pageY - 40}px`;
+        tooltip.style.left = `${e.pageX + 20}px`;
+        tooltip.style.opacity = "1";
+        tooltip.style.fontSize = "1.15rem";
+  
+        // Hide tooltip after 3 seconds
+        setTimeout(() => {
+          tooltip.style.opacity = "0";
+        }, 3000);
+      });
+    });
+  });
